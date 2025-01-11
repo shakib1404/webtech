575c73f8e3da5253c3e819f0d5fbaf2cdd7ba705
Md Shakibul Islam Shakib <126335136+shakib1404@users.noreply.github.com>
2025-01-11 12:25:50 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,30 +1,44 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <form action="index.php" method="post">
-        <label>username:</label><br>
+<form  action="index.php" method="POST">
+        <label >username:</label><br>
         <input type="text" name="username"><br>
-
-        <label>password:</label><br>
+        <label >password:</label><br>
         <input type="password" name="password"><br>
-        <input type="submit" name="submit" value="login"><br>
+        <input type="submit" name="login" value="login">
+
+
+
+
     </form>
-
-
-
 </body>
-
 </html>
 
-
 <?php
-  echo "{$_POST["username"]} <br>";
-  echo "{$_POST["password"]}<br>";
+
+if(isset($_POST["login"])){
+
+
+if(!empty($_POST["username"])  && !empty($_POST["password"]))
+{
+    $_SESSION["username"]=$_POST["username"];
+$_SESSION["password"]=$_POST["password"];
+
+echo $_SESSION["username"]."<br>";
+echo $_SESSION["password"]."<br>";
+ header("Location: session.php");
+}
+}
 ?>
