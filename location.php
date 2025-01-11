15b3fcc998965f89c3284a6fad1fa0be07d0478b
Md Shakibul Islam Shakib <126335136+shakib1404@users.noreply.github.com>
2025-01-11 11:41:39 +0600

    Add files via upload

---
+++ location.php
@@ -0,0 +1,23 @@
+<?php
+
+include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    My Location is hidden!
+    <br>
+    hahaha! atai bastob;
+    <hr>
+</body>
+</html>
+
+<?php
+include("footer.html");
+?>
