fd1546c430c814c7bd9ff2200234e28dfd5c9b0b
Md Shakibul Islam Shakib <126335136+shakib1404@users.noreply.github.com>
2025-01-11 13:03:56 +0600

    Add files via upload

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+$db_server="localhost";
+$db_user="root";
+$db_pass="";
+$db_name="bussinessdb";
+$conn="";
+
+$conn= mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+
+if($conn){
+    echo "You are connected";
+
+}
+
+else{
+    echo "could not  connect";
+}
+
+
+
+
+?>
