f5edec6b5af6ce1fc748fb87399f2f07768740db
Md Shakibul Islam Shakib <126335136+shakib1404@users.noreply.github.com>
2025-01-14 11:03:27 +0600

    Add files via upload

--- jscript.js
+++ jscript.js
@@ -1,14 +1,22 @@
-function loadData(){
+function loadData(callbackF){
     const xhttp=new XMLHttpRequest();
     
     
     xhttp.onload=function(){
-
-        const container=document.getElementById("demo");
-        container.innerHTML=xhttp.responseText;
+        callbackF(this);
     }
 
     xhttp.open("GET","./data/data.txt");
 
-    xhttp.send();
+    xhttp.send(); 
+}
+
+function myCallback1(xhttp){
+    const container=document.getElementById("demo");
+        container.innerHTML=xhttp.responseText;
+}
+
+function myCallback2(xhttp){
+    const container=document.getElementById("demo");
+        container.innerHTML=xhttp.responseText;
 }
