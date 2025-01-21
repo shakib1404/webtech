915b0e330687ad3a4f5dd5506f306f0806f80866
Md Shakibul Islam Shakib <126335136+shakib1404@users.noreply.github.com>
2025-01-21 21:34:29 +0600

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,19 @@
+<?php
+define("HOSTNAME","localhost");
+define("USERNAME","root");
+define("PASSWORD","");
+define("DATABASE","crud_operation");
+
+
+$connection= mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+if(!$connection)
+{
+    die("connection failed");
+}
+else{
+    echo "connection successful";
+}
+
+
+?>
